ab602f441899526dc48addb79f39614fb82d5c4f
unknown <nguyenngan09112000@gmail.com>
2023-06-01 17:29:44 +0700

    commit change

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const Grid = {
 };
 
 function App(props) {
-  const { item, handlers } = useLongPress();
-  const { gridDrag, setGridDrag } = useState();
-  const { gridDrop, setGridDrop } = useState(null);
-  const [dimensions, setDimensions] = React.useState({
+  const { item, handlers, action } = useLongPress();
+  const [gridDrag, setGridDrag] = useState("");
+  const [gridDrop, setGridDrop] = useState("");
+  const [dimensions, setDimensions] = useState({
     width: window.innerWidth / 4,
     height: window.innerHeight / 4,
   });
@@ -22,6 +22,8 @@ function App(props) {
         width: window.innerWidth / 4,
         height: window.innerHeight / 4,
       });
+      setGridDrag("");
+      setGridDrop("");
     }
 
     window.addEventListener("resize", handleResize);
@@ -31,20 +33,67 @@ function App(props) {
     };
   });
 
-  console.log(item, gridDrag);
-  console.log(dimensions);
+  console.log(gridDrag);
 
   useEffect(() => {
-    if (item) {
-      console.log(item.clientX, item.clientY);
+    setGridDrag("");
+  }, [action]);
 
-      if (
-        item?.clientX > 0 &&
-        item?.clientX < dimensions?.width &&
-        item?.clientY > 0 &&
-        item?.clientY < dimensions?.height
-      )
-        setGridDrag("A");
+  useEffect(() => {
+    if (item && action === "longpress") {
+      console.log("check", item.clientX, item.clientY);
+      for (let i = 0; i < 4; i++) {
+        if (
+          item?.clientX > dimensions?.width * i &&
+          item?.clientX < dimensions?.width * (i + 1) &&
+          item?.clientY > 0 &&
+          item?.clientY < dimensions?.height
+        ) {
+          if (i === 0) setGridDrag("A");
+          if (i === 1) setGridDrag("B");
+          if (i === 2) setGridDrag("C");
+          if (i === 3) setGridDrag("D");
+        }
+      }
+      for (let i = 0; i < 4; i++) {
+        if (
+          item?.clientX > dimensions?.width * i &&
+          item?.clientX < dimensions?.width * (i + 1) &&
+          item?.clientY > dimensions?.height &&
+          item?.clientY < dimensions?.height * 2
+        ) {
+          if (i === 0) setGridDrag("E");
+          if (i === 1) setGridDrag("F");
+          if (i === 2) setGridDrag("G");
+          if (i === 3) setGridDrag("H");
+        }
+      }
+      for (let i = 0; i < 4; i++) {
+        if (
+          item?.clientX > dimensions?.width * i &&
+          item?.clientX < dimensions?.width * (i + 1) &&
+          item?.clientY > dimensions?.height * 2 &&
+          item?.clientY < dimensions?.height * 3
+        ) {
+          if (i === 0) setGridDrag("H");
+          if (i === 1) setGridDrag("I");
+          if (i === 2) setGridDrag("J");
+          if (i === 3) setGridDrag("K");
+        }
+      }
+      for (let i = 0; i < 4; i++) {
+        if (
+          item?.clientX > dimensions?.width * i &&
+          item?.clientX < dimensions?.width * (i + 1) &&
+          item?.clientY > dimensions?.height * 2 &&
+          item?.clientY < dimensions?.height * 4
+        ) {
+          if (i === 0) setGridDrag("L");
+          if (i === 1) setGridDrag("M");
+          if (i === 2) setGridDrag("N");
+          if (i === 3) setGridDrag("O");
+        }
+      }
     }
   }, [dimensions, item]);
 
